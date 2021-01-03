7c5d23c98f27068be4efbb66511d6ff2588a4661
kalmai <leozverres@gmail.com>
2021-01-02 22:43:52 -0500

    working on writing practice with io

--- odin_project/io_practice.rb
+++ odin_project/io_practice.rb
@@ -43,6 +43,10 @@ def word_finder
 
   i = 0
   file = File.new(file_name, 'r')
+#  file2 = File.open(file_name, 'r') do |f|
+#    f.gets
+#    puts f.eof?
+#  end
 
   file.each_line do |line|
     i += 1
@@ -59,8 +63,6 @@ def word_finder
   end
 
 end
-
-word_finder
 
 # Create a quiz maker program which asks the user a question. They should be presented with the possible multiple choice answers and allowed to specify the correct answer.
 #
@@ -96,4 +98,103 @@ def quiz_master
   puts "your final score was #{player_score} out of #{total_questions}"
 end
 
-quiz_master
+def file_splitter
+  # Develop an application that takes a significantly large input file and splits it into smaller file chunks. These types of files use to be used quite common back in the earlier days of computing when disks such as floppies were much smaller and couldn’t hold a larger program on their own.
+  #
+  # In order to determine how many files need to be produced, ask the user for the maximum amount of lines that should appear in each output file.
+  #
+  # Where is the input file (please include the path to the file)? [path-to-file]/input.txt
+  # How many lines of text (max) should there be in the split files? 3
+  # The input file has 50 lines of text.
+  #
+  # Each file that is created should have a sequential number assigned to it.
+  #
+  # For a 50 line input file "input.txt" this will produce 17 output files.
+  #
+  # **GENERATING OUTPUT**
+  #
+  # Generating input-1.txt
+  # Generating input-2.txt
+  # Generating input-3.txt
+  puts "enter the file you would like to split 'FizzBuzz.txt'"
+  original_file = gets.strip
+
+  while !File.file?(original_file)
+    puts "please enter a valid file or exit"
+    original_file = gets.strip
+    exit if original_file.eql?("exit")
+  end
+
+  puts "enter how many lines you would like to split #{original_file} by?"
+  lines_of_text_per_file = gets.to_i
+
+  file_lines = `wc -l #{original_file}`.split(" ")[0].to_i
+  number_of_files_to_be_created = ( file_lines / lines_of_text_per_file.to_f ).ceil
+
+  puts "#{original_file} contains #{file_lines} and will produce #{number_of_files_to_be_created} output files"
+  `mkdir delete_this`
+  file_array = (number_of_files_to_be_created).times { |i| File.new("delete_this/input-#{i + 1}", "w") }
+  opened_file = File.open(original_file, "r")
+
+  while !opened_file.eof?
+    file_name_number = 1
+    i = lines_of_text_per_file.dup
+    file = File.new("input-#{file_name_number}", "w")
+    opened_file.each_line do |line|
+      file.puts line
+      i -= 1
+    end
+  end
+
+end 
+
+def fizz_buzz
+  # Create a program to write out the result of FizzBuzz (1 to 300) to a file called FizzBuzz.txt. The file should be written out to the same directory as this README file.
+  #
+  # If the number is divisible by 3 or contains a 3, print “Fizz”
+  # If the number is divisible by 5 or contains a 5, print “Buzz”
+  # If the number is divisible by 3 and 5, print “FizzBuzz”
+  # Otherwise print the number.
+  puts "enter to what digit you want to print fizz-buzz"
+  input = gets.to_i
+  while input > 300 || input < 1
+    puts "please enter an integer between 1 and 300"
+    input = gets.to_i
+  end
+  new_file = File.new("FizzBuzz.txt", "w")
+  for i in 1..input
+    if i % 3 == 0 && i % 5 == 0
+      new_file.puts "FizzBuzz"
+    elsif i % 3 == 0 || i.to_s.split("").include?("3")
+      new_file.puts "Fizz"
+    elsif i % 5 == 0 || i.to_s.split("").include?("5")
+      new_file.puts "Buzz"
+    else 
+      new_file.puts i
+    end
+  end
+end
+
+def method_selector
+  puts "0. word_finder"
+  puts "1. quiz_master"
+  puts "2. fizz_buzz"
+  puts "3. file_splitter"
+  puts "999. exit"
+  input = gets.to_i
+  case input
+  when 0 
+    word_finder()
+  when 1 
+    quiz_master()
+  when 2
+    fizz_buzz
+  when 3
+    file_splitter
+  when 999
+    exit
+  end
+end
+
+#file_splitter
+method_selector
