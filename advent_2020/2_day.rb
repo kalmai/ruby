1ad4eb431448834c05b35c05a55f41d30d7094f2
kalmai <leozverres@gmail.com>
2021-01-22 14:11:53 -0500

    completed bonus challenge for day 2

--- advent_2020/2_day.rb
+++ advent_2020/2_day.rb
@@ -55,4 +55,54 @@ def password_verifier
 
 end
 
-password_verifier
+def pass_word_sub_str_verifier
+  # While it appears you validated the passwords correctly, they don't seem to be what the Official Toboggan Corporate Authentication System is expecting.
+  #
+  # The shopkeeper suddenly realizes that he just accidentally explained the password policy rules from his old job at the sled rental place down the street! The Official Toboggan Corporate Policy actually works a little differently.
+  #
+  # Each policy actually describes two positions in the password, where 1 means the first character, 2 means the second character, and so on. (Be careful; Toboggan Corporate Policies have no concept of "index zero"!) Exactly one of these positions must contain the given letter. Other occurrences of the letter are irrelevant for the purposes of policy enforcement.
+  #
+  # Given the same example list from above:
+  #
+  # 1-3 a: abcde is valid: position 1 contains a and position 3 does not.
+  # 1-3 b: cdefg is invalid: neither position 1 nor position 3 contains b.
+  # 2-9 c: ccccccccc is invalid: both position 2 and position 9 contain c.
+  # How many passwords are valid according to the new interpretation of the policies?
+
+  puts "enter a file to verify the passwords"
+  input = gets.strip
+
+  while !File.file?(input)
+    puts "enter a valid file"
+    input = gets.strip
+  end
+
+  file_reader = File.open(input)
+
+  valid_passwords = 0
+
+  puts "\nprocessing..."
+
+  file_reader.each_line do |line|
+    line_arr = line.split(" ")
+
+    low_high_arr = line_arr[0].split("-")
+    low = low_high_arr[0].to_i
+    high = low_high_arr[1].to_i
+
+    letter = line_arr[1][0].downcase
+
+    first_char = line_arr[2][low - 1]
+    second_char = line_arr[2][high - 1]
+
+    valid_passwords += 1 if first_char.eql?(letter) ^ second_char.eql?(letter)
+
+  end
+  
+  puts "\nthe number of valid passwords is #{valid_passwords}"
+  file_reader.close
+
+end
+
+#password_verifier
+pass_word_sub_str_verifier
