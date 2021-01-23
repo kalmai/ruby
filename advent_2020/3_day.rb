602c881b0604faa980d683e24749d976e3dc13bb
kalmai <leozverres@gmail.com>
2021-01-22 22:37:08 -0500

    created started for day 3 challenge

---
+++ advent_2020/3_day.rb
@@ -0,0 +1,49 @@
+# With the toboggan login problems resolved, you set off toward the airport. While travel by toboggan might be easy, it's certainly not safe: there's very minimal steering and the area is covered in trees. You'll need to see which angles will take you near the fewest trees.
+#
+# Due to the local geology, trees in this area only grow on exact integer coordinates in a grid. You make a map (your puzzle input) of the open squares (.) and trees (#) you can see. For example:
+#
+# ..##.........##.........##.........##.........##.........##.......  --->
+# #..O#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..
+# .#....X..#..#....#..#..#....#..#..#....#..#..#....#..#..#....#..#.
+# ..#.#...#O#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#
+# .#...##..#..X...##..#..#...##..#..#...##..#..#...##..#..#...##..#.
+# ..#.##.......#.X#.......#.##.......#.##.......#.##.......#.##.....  --->
+# .#.#.#....#.#.#.#.O..#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#
+# .#........#.#........X.#........#.#........#.#........#.#........#
+# #.##...#...#.##...#...#.X#...#...#.##...#...#.##...#...#.##...#...
+# #...##....##...##....##...#X....##...##....##...##....##...##....#
+# .#..#...#.#.#..#...#.#.#..#...X.#.#..#...#.#.#..#...#.#.#..#...#.#  --->
+#
+# You start on the open square (.) in the top-left corner and need to reach the bottom (below the bottom-most row on your map).
+#
+# The toboggan can only follow a few specific slopes (you opted for a cheaper model that prefers rational numbers); start by counting all the trees you would encounter for the slope right 3, down 1:
+#
+# From your starting position at the top-left, check the position that is right 3 and down 1. Then, check the position that is right 3 and down 1 from there, and so on until you go past the bottom of the map.
+#
+# The locations you'd check in the above example are marked here with O where there was an open square and X where there was a tree:
+#
+# You start on the open square (.) in the top-left corner and need to reach the bottom (below the bottom-most row on your map).
+#
+# The toboggan can only follow a few specific slopes (you opted for a cheaper model that prefers rational numbers); start by counting all the trees you would encounter for the slope right 3, down 1:
+#
+# From your starting position at the top-left, check the position that is right 3 and down 1. Then, check the position that is right 3 and down 1 from there, and so on until you go past the bottom of the map.
+#
+# The locations you'd check in the above example are marked here with O where there was an open square and X where there was a tree:
+
+def tree_nav
+  puts "enter the input file"
+  input = gets.strip
+
+  while !File.file?(input)
+    puts "#{input} is not a valid file, please enter a valid file"
+    input = gets.strip
+  end
+
+  file_reader = File.open(input)
+
+  file_reader.each_line do |line|
+  end
+
+  
+
+end
