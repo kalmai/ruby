4d31aa3129f918304ae6fc4db0d84f1bfeb0b88d
kalmai <leozverres@gmail.com>
2021-01-25 21:56:29 -0500

    code seems to work, answer is still wrong...

--- advent_2020/3_day.rb
+++ advent_2020/3_day.rb
@@ -32,18 +32,8 @@
 
 require 'pry'
 
-def tree_nav(horizontal_amount, vertical_amount, input = nil)
-  if input.nil?
-    puts "enter the input file"
-    input = gets.strip
-  end
-
-  while !File.file?(input)
-    puts "#{input} is not a valid file, please enter a valid file"
-    input = gets.strip
-  end
-
-  file_reader = File.open(input)
+def tree_nav(horizontal_amount, vertical_amount, file)
+  file_reader = File.open(file)
 
   position = 0 
   down_count = vertical_amount
@@ -51,31 +41,54 @@ def tree_nav(horizontal_amount, vertical_amount, input = nil)
 
   file_reader.each_line do |line|
     forest = line.strip
-#    binding.pry if vertical_amount == 2
 
     if down_count == vertical_amount
       temp_position = position > forest.length ? position % forest.length : position
       value_at_position = forest[temp_position]
 
+#      binding.pry if position == 0 
       tree_count += 1 if value_at_position.eql?('#')
       position += horizontal_amount
       down_count = 1 if vertical_amount > 1
-      next
+    else
+      down_count += 1
     end
-    down_count += 1
   end
 
   file_reader.close
 
-  puts "you encountered #{tree_count} trees descending the slope that was #{`wc -l #{input}`.split(" ")[0].to_i} long by moving horizontally #{horizontal_amount} and down by #{vertical_amount}"
+  puts position 
+  puts "you encountered #{tree_count} trees descending the slope that was #{`wc -l #{file}`.split(" ")[0].to_i} long by moving horizontally #{horizontal_amount} and down by #{vertical_amount}"
 
   return tree_count
 end
 
-a = tree_nav(1,1,'input-3.txt')
-b = tree_nav(3,1,'input-3.txt')
-c = tree_nav(5,1,'input-3.txt')
-d = tree_nav(7,1,'input-3.txt')
-e = tree_nav(1,2,'input-3.txt')
+# Time to check the rest of the slopes - you need to minimize the probability of a sudden arboreal stop, after all.
+#
+# Determine the number of trees you would encounter if, for each of the following slopes, you start at the top-left corner and traverse the map all the way to the bottom:
+#
+# Right 1, down 1.
+# Right 3, down 1. (This is the slope you already checked.)
+# Right 5, down 1.
+# Right 7, down 1.
+# Right 1, down 2.
+# In the above example, these slopes would find 2, 7, 3, 4, and 2 tree(s) respectively; multiplied together, these produce the answer 336.
+#
+# What do you get if you multiply together the number of trees encountered on each of the listed slopes?
+#
+# the answer to the previous slope was 270
+
+puts "enter the input file"
+input = gets.strip
+while !File.file?(input)
+  puts "#{input} is not a valid file, please enter a valid file"
+  input = gets.strip
+end
+
+a = tree_nav(1,1,input)
+b = tree_nav(3,1,input)
+c = tree_nav(5,1,input)
+d = tree_nav(7,1,input)
+e = tree_nav(1,2,input)
 
 puts "when you multipy all slopes together you get #{a * b * c * d * e}"
