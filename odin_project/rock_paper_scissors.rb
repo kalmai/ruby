56da59ed936a5bdc0c08585235c92d226f3c10ef
kalmai <leozverres@gmail.com>
2021-01-27 10:52:26 -0500

    created rock_paper_scissors game to show friend coding, not related to course

---
+++ odin_project/rock_paper_scissors.rb
@@ -0,0 +1,82 @@
+require 'pry'
+
+def winner?(cpu_choice, human_choice, tracker)
+  loss_message = ["you lose", "you've lost", "git gud, you've lost", "you lost :("] 
+  win_message = ["*pop-up voice* congratulations... you've won!", "you win!", "you've won", "one small step for man"]
+  if cpu_choice.eql?(human_choice)
+    puts "it's a tie"
+  elsif human_choice.eql?('r') && cpu_choice.eql?('p')
+    tracker.loser
+    puts loss_message.sample
+  elsif human_choice.eql?('p') && cpu_choice.eql?('s')
+    tracker.loser
+    puts loss_message.sample
+  elsif human_choice.eql?('s') && cpu_choice.eql?('r')
+    tracker.loser
+    puts loss_message.sample
+  elsif cpu_choice.eql?('r') && human_choice.eql?('p')
+    tracker.winner
+    puts win_message.sample
+  elsif cpu_choice.eql?('p') && human_choice.eql?('s')
+    tracker.winner
+    puts win_message.sample
+  elsif cpu_choice.eql?('s') && human_choice.eql?('r')
+    tracker.winner
+    puts win_message.sample
+  end
+end
+
+class Score_Tracker
+  attr_accessor :wins, :losses
+
+  def initialize
+    @wins = 0
+    @losses = 0
+  end
+
+  def winner
+    self.wins += 1
+  end
+
+  def loser
+    self.losses += 1
+  end
+
+end
+
+def play(tracker)
+  puts "rock, paper, or scissors? (you much enter: r,p,s)"
+  input = gets.strip
+
+  cpu_answer = %w( r p s )
+  char_to_string = {
+    "r" => "rock",
+    "s" => "scissors",
+    "p" => "paper"
+  }
+
+  while !cpu_answer.include?(input)
+    puts "please enter a valid input (r,p,s)"
+    input = gets.strip
+  end
+
+  cpu_choice = cpu_answer.sample
+
+  puts "you selected #{char_to_string[input]} while the computer selected #{char_to_string[cpu_choice]}"
+  winner?(cpu_choice, input, tracker)
+  puts "\nwins: #{tracker.wins}, losses: #{tracker.losses}"
+
+  puts "play again? (y/n)"
+  play_again = gets.strip
+
+  while !%w(y n).include?(play_again)
+    puts "please enter 'y' or 'n'"
+    play_again = gets.strip
+  end
+
+  play_again.eql?('y') ? play(tracker) : exit
+
+end
+
+tracker = Score_Tracker.new
+play(tracker)
