ddadc7b1863b4223e781d26856ca0d07347bfbd4
kalmai <leozverres@gmail.com>
2021-02-14 00:19:09 -0500

    completed cleaning of the phone numbers part of event_manager project

--- odin_project/event_manager/lib/event_manager.rb
+++ odin_project/event_manager/lib/event_manager.rb
@@ -1,6 +1,35 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
 require 'erb'
+require 'pry'
+
+def dirty_number?(num)
+  invalids = %w( ( ) - . / )
+  num.split('').any? { |c| bad_char = c if invalids.include? (c) } 
+end
+
+def clean_homephone(phone_num)
+  invalids = %w( ( ) - . / )
+  bad_char = ""
+  phone_num.split('').any? { |c| bad_char = c if invalids.include? (c) } 
+  phone_num.gsub!(bad_char, '')
+  return phone_num
+end
+
+
+def clean_phone_numbers(phone_num)
+  while dirty_number?(phone_num)
+    phone_num = clean_homephone(phone_num)
+  end
+  phone_num = phone_num.split(' ').join
+  if phone_num.length < 10 || phone_num.length > 11 || ( phone_num.length == 11 && !phone_num[0].eql?('1') ) 
+    "invalid phone number"
+  elsif phone_num.length == 10
+    phone_num
+  elsif phone_num.length == 11 && phone_num[0].eql?('1')
+    phone_num[1..phone_num.length - 1]
+  end
+end
 
 def thank_you_letters(id, form_letter)
   Dir.mkdir('output') unless Dir.exists?('output')
@@ -44,10 +73,13 @@ contents.each do |row|
   id = row[0]
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
+
+  phone_number = clean_phone_numbers(row[:homephone])
   
   legislators = zip_legislators(zipcode)
 
   form_letter = erb_template.result(binding)
+  puts form_letter
 
-  thank_you_letters(id, form_letter)
+#  thank_you_letters(id, form_letter)
 end
