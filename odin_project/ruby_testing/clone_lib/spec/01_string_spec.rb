da0f7b487ddd9333c898532673c3eea3be516f86
kalmai <leozverres@gmail.com>
2021-06-07 22:05:10 -0400

    completed lessons 1-4 for testing practice

--- odin_project/ruby_testing/clone_lib/spec/01_string_spec.rb
+++ odin_project/ruby_testing/clone_lib/spec/01_string_spec.rb
@@ -39,18 +39,21 @@ end
 
 describe String do
   # Create a let variable that will pass the first test.
+  let(:favorite_food) { String.new('tacos') }
 
   # remove the 'x' before running this test
-  xit 'is equal to tacos' do
+  it 'is equal to tacos' do
     expect(favorite_food).to eq('tacos')
   end
 
   # remove the 'x' before running this test
   context 'when favorite food is updated' do
     # Change the favorite_food let variable.
+    let(:favorite_food) { String.new('pizza') }
 
-    xit 'updates the favorite food' do
+    it 'updates the favorite food' do
       # Write a test that will pass.
+      expect(favorite_food).to eql('pizza')
     end
   end
 end
--- odin_project/ruby_testing/clone_lib/spec/02_array_spec.rb
+++ odin_project/ruby_testing/clone_lib/spec/02_array_spec.rb
@@ -72,27 +72,30 @@ end
 describe Array do
   context 'when updating an implicit subject' do
     # remove the 'x' before running this test
-    xit 'is empty' do
+    it 'is empty' do
       # Write a test to expect the subject to be empty.
+      expect(subject).to be_empty
     end
 
     # remove the 'x' before running this test
-    xit 'updates length to 1' do
+    it 'updates length to 1' do
       # Update the implicit subject to make this test pass.
+      subject.push(1)
       expect(subject.length).to eq(1)
     end
   end
 
   context 'when using one let variable on two tests' do
     # Make a let variable that will pass both tests.
+    let(:lucky_numbers) { [1,1,40] }
 
     # remove the 'x' before running this test
-    xit 'has length of 3' do
+    it 'has length of 3' do
       expect(lucky_numbers.length).to eq(3)
     end
 
     # remove the 'x' before running this test
-    xit 'has sum of 42' do
+    it 'has sum of 42' do
       expect(lucky_numbers.sum).to eq(42)
     end
   end
