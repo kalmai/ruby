4530e0d4eebc163ee45475a1c382aa54c5f12047
kalmai <leozverres@gmail.com>
2021-10-31 19:30:39 -0400

    completed p2 of d4

--- advent_2020/4_day/4_day.rb
+++ advent_2020/4_day/4_day.rb
@@ -28,17 +28,56 @@ module PassPortCounter
   end
 
   def is_valid_north_pole_passport?(hsh)
-    ecl = hsh.key?(:ecl)
-    pid = hsh.key?(:pid)
-    eyr = hsh.key?(:eyr)
-    hcl = hsh.key?(:hcl)
-    byr = hsh.key?(:byr)
-    iyr = hsh.key?(:iyr)
-    cid = hsh.key?(:cid)
-    hgt = hsh.key?(:hgt)
+    if hsh.values.none? { |v| v.nil? } && hsh.keys.each { |k| [:ecl, :pid, :eyr, :hcl, :byr, :iyr, :hgt].include?(k) } 
+      ecl = ecl?(hsh[:ecl])
+      pid = pid?(hsh[:pid])
+      eyr = eyr?(hsh[:eyr])
+      hcl = hcl?(hsh[:hcl])
+      byr = byr?(hsh[:byr])
+      iyr = iyr?(hsh[:iyr])
+      hgt = hgt?(hsh[:hgt])
 
-    return ecl && pid && eyr && hcl && byr && iyr && hgt
+      return ecl && pid && eyr && hcl && byr && iyr && hgt
+    end
+    return false
+  end
 
+  def ecl?(ecl)
+    ecl && %w(amb blu brn gry grn hzl oth).include?(ecl)
+  end
+
+  def pid?(pid)
+    pid && pid.length == 9 && pid.to_i > 0
+  end
+
+  def eyr?(eyr)
+    eyr && eyr.length == 4 && eyr.to_i.between?(2020, 2030)
+  end
+
+  def hcl?(hcl)
+    hcl && hcl[0].eql?("#") && hcl.length == 7 && hcl[1...].split("").all? { |c| /[0-9a-f]/.match?(c) }
+  end
+
+  def byr?(byr)
+    byr && byr.length == 4 && byr.to_i.between?(1920, 2002)
+  end
+
+  def iyr?(iyr)
+    iyr && iyr.length == 4 && iyr.to_i.between?(2010, 2020)
+  end
+
+  def hgt?(hgt)
+    if hgt
+      units = hgt[-2..]
+      num = hgt[0..hgt.size - 3]
+      return true if units.eql?("cm") && num.to_i.between?(150, 193)
+      return true if units.eql?("in") && num.to_i.between?(59, 76)
+    end
+    return false
+  end
+
+  def is_between_values?(low, high, value)
+    value.to_i.between?(low, high)
   end
 
 end
