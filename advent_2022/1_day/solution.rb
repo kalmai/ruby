cfd15ad461e20382e0a690878535eea19f15f5b2
kalmai <leozverres@gmail.com>
2022-12-01 23:16:08 -0500

    solved day1 part2

--- advent_2022/1_day/solution.rb
+++ advent_2022/1_day/solution.rb
@@ -3,13 +3,14 @@ require 'pry'
 
 def solution(file_name)
   data = File.read(file_name)
-  most_calories = -9999
+  most_calories = [0, -1, -2]
   elf_calories = data.split("\n\n")
   elf_calories.each do |str|
     total_calories = str.split("\n").map(&:to_i).sum
-    most_calories = total_calories if total_calories > most_calories
+    least_most_calories = most_calories.min
+    most_calories[most_calories.index(least_most_calories)] = total_calories if total_calories > most_calories.min
   end
-  most_calories
+  most_calories.sum
 end
 
 # puts solution('sample.txt')
