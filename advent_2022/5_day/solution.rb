8e0bb3426762e813a624c1831169f32bdbd9de60
kalmai <zacharykolker93@gmail.com>
2022-12-06 19:47:50 -0500

    solved day5 part1

--- advent_2022/5_day/solution.rb
+++ advent_2022/5_day/solution.rb
@@ -8,11 +8,26 @@ def solution(filename = 'input.txt')
            .select { |s| ('0'..'9').to_a.include?(s) }
            .map(&:to_i)
            .to_h { |v| [v, []] }
-  i = 0
   crates.split("\n").each do |crate|
-    a = crate.chars.each_slice(4).map(&:join)
-    binding.pry
+    crate.chars.each_slice(4).map(&:join).each.with_index(1) do |str, i|
+      box = str.scan(/\w+/)
+      stacks[i].unshift(box) unless box.empty?
+    end
   end
+
+  moves = moves.split("\n").map do |str|
+    move = str.split(' ').select { |s| s.to_i > 0 }.map(&:to_i)
+    Move.new(*move)
+  end
+
+  moves.each do |m|
+    start_stack = stacks[m.start_pos]
+    moving_boxes = start_stack.slice!((start_stack.length - m.boxes)..)
+    moving_boxes.reverse.each { |b| stacks[m.end_pos].push(b) }
+  end
+  stacks.values.map(&:last).join
 end
 
-puts solution('sample.txt')
+Move = Struct.new(:boxes, :start_pos, :end_pos)
+
+puts solution # ('sample.txt')
