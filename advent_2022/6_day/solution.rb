2dba91852330bd13f3ac59e798ca068f9e0ac7b4
kalmai <zacharykolker93@gmail.com>
2022-12-08 00:06:00 -0500

    solved day6 part2

--- advent_2022/6_day/solution.rb
+++ advent_2022/6_day/solution.rb
@@ -2,9 +2,9 @@ def solution(filename = 'input.txt')
   signal = File.read(filename).strip
   packet_start = []
   signal.split('').each.with_index(1) do |char, i|
-    packet_start.shift if packet_start.size == 4
+    packet_start.shift if packet_start.size == 14
     packet_start.push(char)
-    return i if packet_start.tally.values.size == 4
+    return i if packet_start.tally.values.size == 14
   end
 end
 
